d5cb0ad3beb989f1e601f19bdca65b99472b840b
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-06-16 14:57:36 +0530

    updated

--- ragws/src/ragapp/utils/rag_engine.py
+++ ragws/src/ragapp/utils/rag_engine.py
@@ -4,7 +4,11 @@
 
 import os
 from dotenv import load_dotenv
+from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_classic.chains import RetrievalQA
 env_path= os.path.join(os.path.dirname(__file__), '..','.env')
 load_dotenv(env_path)
 
@@ -14,3 +18,25 @@ def load_data_from_vector_db():
         raise ValueError("persist_directory is not set in the environment variables.")
     #read embeddings from persistent directory
     embeddings= HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    #read from vector database
+    vector_db = Chroma(persist_directory=persistent_directory, embedding_function=embeddings)
+    retriever = vector_db.as_retriever(
+        search_kwargs={"k": 3}
+    )
+    return retriever
+
+def delivery_query(prompt,retriever):
+    #define open ai llm
+    llm = ChatOpenAI(
+        model="gpt-4o-mini",
+        temperature=0
+    )
+    rag_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        retriever=retriever,
+        return_source_documents=True,
+        chain_type="stuff",
+        #chain_type_kwargs={"prompt": prompt_template}
+    )
+
+    return rag_chain
