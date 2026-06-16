d571aee849e9d6982062619b9ae87d7608bcd329
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-06-16 14:14:20 +0530

    updated

--- ragws/src/ragapp/data_ingestor/data_input.py
+++ ragws/src/ragapp/data_ingestor/data_input.py
@@ -2,7 +2,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_classic.document_loaders import Docx2txtLoader, TextLoader,PyPDFLoader
-
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 env_path = os.path.join(os.path.dirname(__file__),'..', '.env')
 load_dotenv(env_path)
 
@@ -21,3 +21,9 @@ def load_documents(dir_path):
             loader=Docx2txtLoader(file_path)
             documents.extend(loader.load())
     return documents
+
+def create_chunks(documents, chunk_size=1000, chunk_overlap=0):
+    
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    chunks = text_splitter.split_documents(documents)
+    return chunks
