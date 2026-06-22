676fea91c69dacd53f8978276cdc038d264d032b
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-06-22 14:50:58 +0530

    updated

---
+++ tripmateapp/src/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+//import image from '../assets/logo.jpg';
+import TravelAppLogo from '../assets/logo.jpg'
+//logo component
+const Logo = () => {
+  return (
+    <img src={TravelAppLogo} alt="Logo" />
+  );
+}
+
+export default Logo;
